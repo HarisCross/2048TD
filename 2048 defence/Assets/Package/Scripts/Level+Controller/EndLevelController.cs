6cad55efd6ab3de389507e25b6618da1b435b4a8
HarisCross <hariscross@gmail.com>
2019-05-24 19:27:48 +0100

    24/05/2019
    working on win conditions and the math required to get the stars to fill up the right amount. also got the mask working w/ simple shapes to cahgen later

--- 2048 defence/Assets/Package/Scripts/Level+Controller/EndLevelController.cs
+++ 2048 defence/Assets/Package/Scripts/Level+Controller/EndLevelController.cs
@@ -13,16 +13,23 @@ public class EndLevelController : MonoBehaviour
 
     [Header("Win Condition arrays")]
 
-    private int[] winBoundariesTimeTaken; //in seconds
-    private int[] winBoundariesGridMovements; //amount of swipes used on the grids
+    private int[] winBoundariesTimeTaken; //in seconds, [0] is quickest = 3 stars, [2] is slowest = 1 star
     private int[] winBoundariesGridExports; //amount of times a value is exported
+    private int[] winBoundariesGridMovements; //amount of swipes used on the grids
 
     [Header("Actual Completion Variables")]
     private float currentTimeTakenToFinishAllWaves = 0f;
     private int   currentExportCounter = 0;
     private int   currentAmountOfTimesGridMoved = 0;
 
+    [Header("Images to Move For Stars")]
+    public GameObject leftImage;
+    public GameObject MiddleImage;
+    public GameObject RightImage;
 
+    [Header("Other")]
+
+    public float[] intHeightVars =  new float[] {-999f,-666f,-333f,0f };//lowest to heighset
 
     void Start()
     {
@@ -70,5 +77,72 @@ public class EndLevelController : MonoBehaviour
 
         levelEndSlides.SetActive(true);
 
+    }
+
+    private void CalcHeightForTimeTaken(float currentValueForSection,int[] arrayAssociatedWithCurrValue)//be passed the actual value and the array associated with it
+    {
+        //use time given and compare to time array and move the according background up cerain amount depending on score percentage
+
+        float lowBoundary, highBoundary;// find out what boundarys the score given is between
+        float returnHeightToMoveToo;
+
+        if(currentValueForSection < arrayAssociatedWithCurrValue[2])//is time taken above the LOWEST boundary
+        {
+            //time taken is above lowest boundary
+
+            if (currentValueForSection < arrayAssociatedWithCurrValue[1])//is time taken above the MEDIUM boundary
+            {
+                if (currentValueForSection < arrayAssociatedWithCurrValue[0])//is time taken above the HIGHEST boundary
+                {
+                    //above 3 stars###, doesnt need to find amount to raise as it will fill all stars anyway
+
+                    returnHeightToMoveToo = intHeightVars[3];
+
+
+                    StartCoroutine(RaiseImageGivenByFloatGiven ( leftImage, returnHeightToMoveToo));
+
+                }
+                else
+                {
+                    //between 2 and 3 stars#####needs to find amount to raise image by##
+
+                    returnHeightToMoveToo = ReturnHeightToRaiseTheImageToo(currentValueForSection, arrayAssociatedWithCurrValue[1], arrayAssociatedWithCurrValue[2],intHeightVars[2], intHeightVars[3]);
+                    StartCoroutine(RaiseImageGivenByFloatGiven(leftImage, returnHeightToMoveToo));
+                }
+
+            }
+            else
+            {
+                //between 1 and 2 stars#####needs to find amount to raise image by##
+                returnHeightToMoveToo = ReturnHeightToRaiseTheImageToo(currentValueForSection, arrayAssociatedWithCurrValue[0], arrayAssociatedWithCurrValue[1],intHeightVars[1], intHeightVars[2]);
+                StartCoroutine(RaiseImageGivenByFloatGiven(leftImage, returnHeightToMoveToo));
+            }
+
+
+        }
+        else
+        {
+            //time taken is BELOW lowest boundary,##needs to find amount to raise image by##
+            returnHeightToMoveToo = ReturnHeightToRaiseTheImageToo(currentValueForSection, 0, arrayAssociatedWithCurrValue[0],intHeightVars[0],intHeightVars[1]);
+            StartCoroutine(RaiseImageGivenByFloatGiven(leftImage, returnHeightToMoveToo));
+        }
+
+    }
+    private float ReturnHeightToRaiseTheImageToo(float actualValue,float lowerBoundaryValue,float higherBoundaryValue,float lowerBoundaryHeight, float higherBoundaryHeight)
+    {
+        //takes the actual value obtained and uses the two boundarys it is between to determine the height the image should be raised by depending on the actual value
+
+        float varToReturn = 0f;
+
+        varToReturn = (actualValue - lowerBoundaryValue) / (higherBoundaryValue - lowerBoundaryValue); // returns between 0 -1 range, is % of how far the value is between the boundarys
+
+        //use % obtained above to find out how far to raise the height too between two values
+
+
+        return varToReturn;
+    }
+    private IEnumerator RaiseImageGivenByFloatGiven(GameObject imageToRaise,float amountToRaiseToo)
+    {
+        yield return new WaitForEndOfFrame();//just here to stop the red wiggly line of annoynace
     }
 }
